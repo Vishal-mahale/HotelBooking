3fb25a6eeb13fe519b359f5516a84b9d03c14de8
Vishal-mahale <vmahale799@gmail.com>
2025-07-09 18:00:06 +0530

    Database connection and user controller is created.

---
+++ server/controllers/clerkWebhooks.js
@@ -0,0 +1,50 @@
+import User from '../models/User.js'
+import { Webhook } from 'svix'
+
+const clerkWebHooks = async (req, res) => {
+  try {
+    const webhook = new webhook(process.env.CLERK_WEBHOOK_SECRET)
+    // Getting headers
+    const headers = {
+      'svix-id': req.headers['svix-id'],
+      'svix-timestamp': req.headers['svix-timestamp'],
+      'svix-signature': req.headers['svix-signature']
+    }
+    // Verify the webhook headers
+    await webhook.varify(JSON.stringify(req.body), headers)
+    
+    // Getting data from request body
+    const { data, type } = req.body
+
+    const userData = {
+      _id: data.id,
+      email: data.email_addresses[0].email_address,
+      username: data.first_name + ' ' + data.last_name,
+      image: data.image_url
+    }
+
+    // Switch case for the different Events
+
+    switch (type) {
+      case 'user.created': {
+        User.create(userData)
+        break
+      }
+      case 'user.updated': {
+        User.findByIdAndUpdate(data.id, userData)
+        break
+      }
+      case 'user.deleted': {
+        await User.findByIdAndDelete(data.id)
+        break
+      }
+      default:
+        break
+    }
+    res.json({ success: true, message: 'Webhook received successfully' })
+  } catch (error) {
+    res.json({ success: false, message: error.message })
+  }
+}
+
+export default clerkWebHooks
--- server/server.js
+++ server/server.js
@@ -1,1 +1,39 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import { clerkMiddleware } from '@clerk/express'
+
+
+connectDB(); // Connect to MongoDB
+
+const app = express(); //to enable cross origin resource sharing
+
+app.use(cors());
+app.use(clerkMiddleware())
+app.use(express.json()); // to parse JSON bodies
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
+
+
+
+
+
+
+
+
+
+
+
 console.log("This is server js file");
